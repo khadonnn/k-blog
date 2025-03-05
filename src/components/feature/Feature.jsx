0ec8c520c392bd2437734cdfff8732430bf23f92
khadonnn <nguyendinhdongkha@gmail.com>
2025-03-06 01:33:13 +0700

    style: user welcome

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,12 +1,25 @@
+"use client";
 import React from "react";
 import styles from "./feature.module.css";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
+
 const Feature = () => {
+    const { data: session, status } = useSession();
+
+    console.log(session);
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>
-                <b>Hey, khadon!</b> Discover my stories and creative ideas.
-            </h1>
+            {status === "authenticated" ? (
+                <h1 className={styles.title}>
+                    <b>Hey, {session?.user?.name}!</b> Discover your stories and
+                    creative ideas.
+                </h1>
+            ) : (
+                <h1 className={styles.title}>
+                    <b>Welcome K-Blog!</b> Login to write blogs and stories.
+                </h1>
+            )}
             <div className={styles.post}>
                 <div className={styles.imgContainer}>
                     <Image src='/p1.jpeg' alt='' fill />
